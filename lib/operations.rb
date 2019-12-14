d700466a6dfe0d222e523ed97ee7e0ac86db3c49
pledgedavis <pledgedavis14@gmail.com>
2019-12-14 06:58:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -5,14 +5,18 @@ if speed >= 60
 elsif speed <= 40 
   true
 else speed != 50
-  true
+  false
  end
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed > 60 
+	  true
+	elseif speed < 40 
+	  true
+	  
 end
 	
 
